c1f6c2e9dbb657b8bed0c5b04731873db31a4bed
esakat <esaka.tom@gmail.com>
2018-04-30 16:08:01 +0900

    木をたどる関数を作成

--- test/chap3/node1.rb
+++ test/chap3/node1.rb
@@ -6,3 +6,11 @@ leaf_d = ["葉D"]
 node2 = ["節2", leaf_a, leaf_b]
 node3 = ["節3", leaf_c, leaf_d]
 node1 = ["節1", node2, node3]
+
+p(node1[0])
+p(node1[1])
+p(node1[1][0])
+p(node1[1][1])
+
+leaf_X = node1[1][1]
+p(leaf_X[1]) # nil
