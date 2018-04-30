84c1e812677976a693f0ffa960029e76e5693858
esakat <esaka.tom@gmail.com>
2018-04-30 15:58:51 +0900

    木をrubyで表現

---
+++ test/chap3/ary0.rb
@@ -0,0 +1,6 @@
+ary = [10, [20, 30]]
+p(ary[0])
+p(ary[1])
+right = ary[1]
+p(right[0])
+p(ary[1][0])
--- test/chap3/try-pair.rb
+++ test/chap3/try-pair.rb
@@ -1,2 +1,14 @@
 pair = [10, 20]
 p(pair)
+left = pair[0]
+p(left)
+right = pair[1]
+p(right)
+##
+ary = [10, 20, 30]
+p(ary[0])
+p(ary[1])
+p(ary[2])
+##
+empty = []
+p(empty)
