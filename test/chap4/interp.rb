3a5ccac8f7131153bca9a99a1da51ade355686e4
esakat <esaka.tom@gmail.com>
2018-05-01 13:20:40 +0900

    電卓の完成

--- test/chap4/interp.rb
+++ test/chap4/interp.rb
@@ -1,6 +1,35 @@
 require "minruby"
 
-
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when tree[0] = "+"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left + right
+  when tree[0] = "-"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left - right
+  when tree[0] = "*"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left * right
+  when tree[0] = "/"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left / right
+  when tree[0] = "%"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left % right
+  when tree[0] = "**"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left ** right
+  end
+end
 
 # 計算式の文字列を読み込む
 str = gets
@@ -9,7 +38,7 @@ str = gets
 tree = minruby_parse(str)
 
 # 計算を実行
-# answer = ...
+answer = evaluate(tree)
 
 # 計算結果を出力
 p(answer)
