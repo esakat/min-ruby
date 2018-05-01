dbe791e7b1cdd991067fa40cd9db5bf3a1be7236
esakat <esaka.tom@gmail.com>
2018-05-01 12:45:08 +0900

    関数のテストを追加

---
+++ test/chap4/function.rb
@@ -0,0 +1,7 @@
+def add(x, y)
+  p "Addition!"
+  x + y # 最後の行の値が返される
+end
+
+answer = add(40, 2)
+p(answer)
--- test/chap4/interp.rb
+++ test/chap4/interp.rb
@@ -2,3 +2,5 @@ require "minruby"
 
 tree = minruby_parse("1 + 2 * 4")
 p(tree)
+
+pp(minruby_parse("(1 + 2) / 3 * 4 * (56 / 7 + 8 +9)"))
