eb667e6225ea341e32334bf8d6b4ddc8e7eadcdf
esakat <esaka.tom@gmail.com>
2018-05-14 08:41:50 +0900

    env chap5

---
+++ test/chap5/apply_var.rb
@@ -0,0 +1,5 @@
+require "minRuby"
+pp(minruby_parse("
+x = 1
+y = 2 * 3
+"))
--- test/chap5/eval.rb
+++ test/chap5/eval.rb
@@ -1,31 +1,35 @@
 require "minruby"
 
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when "lit"
     tree[1]
   when tree[0] = "+"
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when tree[0] = "-"
-    evaluate(tree[1]) - evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when tree[0] = "*"
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when "/"
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when "stmts"
     i = 1
     last = nil
     while tree[i] != nil
-      last = evaluate(tree[i])
+      last = evaluate(tree[i], env)
       i = i + 1
     end
     last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
   when "func_call"
-    p(evaluate(tree[2]))
+    p(evaluate(tree[2], env))
   end
 end
 
 str = minruby_load()
 tree = minruby_parse(str)
-answer = evaluate(tree)
-p(answer)
+env = {}
+evaluate(tree, env)
