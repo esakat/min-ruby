ba9dbe548289e47930cd78be7c6253455605a7c2
esakat <esaka.tom@gmail.com>
2018-09-16 00:43:01 +0900

    if式評価を追加

---
+++ test/chap6/eval.rb
@@ -0,0 +1,51 @@
+require "minruby"
+
+def evaluate(tree, env)
+  case tree[0]
+  when "if"
+    if evaluate(tree[1], env)
+      evaluate(tree[2], env)
+    else
+      evaluate(tree[3], env)
+    end
+  when "lit"
+    tree[1]
+  when tree[0] = "+"
+    evaluate(tree[1], env) + evaluate(tree[2], env)
+  when tree[0] = "-"
+    evaluate(tree[1], env) - evaluate(tree[2], env)
+  when tree[0] = "*"
+    evaluate(tree[1], env) * evaluate(tree[2], env)
+  when "/"
+    evaluate(tree[1], env) / evaluate(tree[2], env)
+  when tree[0] = "%"
+    evaluate(tree[1], env) % evaluate(tree[2], env)
+  when tree[0] = "**"
+    evaluate(tree[1], env) ** evaluate(tree[2], env)
+  when tree[0] = ">"
+    evaluate(tree[1], env) > evaluate(tree[2], env)
+  when tree[0] = "<"
+    evaluate(tree[1], env) < evaluate(tree[2], env)
+  when tree[0] = "=="
+    evaluate(tree[1], env) == evaluate(tree[2], env)
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i], env)
+      i = i + 1
+    end
+    last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
+  when "func_call"
+    p(evaluate(tree[2], env))
+  end
+end
+
+str = minruby_load()
+tree = minruby_parse(str)
+env = {}
+evaluate(tree, env)
---
+++ test/chap6/experiment4.rb
@@ -0,0 +1,7 @@
+if 0 == 0
+  p("true")
+else
+  p("false")
+end
+
+p("end if statement")
