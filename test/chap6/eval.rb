4385e00f3d6fc98ed4fc4b9d8d116ada71c8dc18
esakat <esaka.tom@gmail.com>
2018-09-16 01:24:04 +0900

    6章の演習問題

--- test/chap6/eval.rb
+++ test/chap6/eval.rb
@@ -12,6 +12,11 @@ def evaluate(tree, env)
     while evaluate(tree[1], env)
       evaluate(tree[2], env)
     end
+  when "while2" # begin-endのwhile
+    evaluate(tree[2], env)
+    while evaluate(tree[1], env)
+      evaluate(tree[2], env)
+    end
   when "lit"
     tree[1]
   when "+"
