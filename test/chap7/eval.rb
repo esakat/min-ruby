593ac91bca561f90bb79236dc2200ef2078b5c4b
esakat <esaka.tom@gmail.com>
2018-09-16 17:38:29 +0900

    組み込み関数を実装した

---
+++ test/chap7/eval.rb
@@ -0,0 +1,72 @@
+require "minruby"
+
+def evaluate(tree, genv ,lenv)
+  case tree[0]
+  when "if"
+    if evaluate(tree[1], genv, lenv)
+      evaluate(tree[2], genv, lenv)
+    else
+      evaluate(tree[3], genv, lenv)
+    end
+  when "while"
+    while evaluate(tree[1], genv, lenv)
+      evaluate(tree[2], genv, lenv)
+    end
+  when "while2" # begin-endのwhile
+    evaluate(tree[2], genv, lenv)
+    while evaluate(tree[1], genv, lenv)
+      evaluate(tree[2], genv, lenv)
+    end
+  when "lit"
+    tree[1]
+  when "+"
+    evaluate(tree[1], genv, lenv) + evaluate(tree[2], genv, lenv)
+  when "-"
+    evaluate(tree[1], genv, lenv) - evaluate(tree[2], genv, lenv)
+  when "*"
+    evaluate(tree[1], genv, lenv) * evaluate(tree[2], genv, lenv)
+  when "/"
+    evaluate(tree[1], genv, lenv) / evaluate(tree[2], genv, lenv)
+  when "%"
+    evaluate(tree[1], genv, lenv) % evaluate(tree[2], genv, lenv)
+  when "**"
+    evaluate(tree[1], genv, lenv) ** evaluate(tree[2], genv, lenv)
+  when ">"
+    evaluate(tree[1], genv, lenv) > evaluate(tree[2], genv, lenv)
+  when "<"
+    evaluate(tree[1], genv, lenv) < evaluate(tree[2], genv, lenv)
+  when "=="
+    evaluate(tree[1], genv, lenv) == evaluate(tree[2], genv, lenv)
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i], genv, lenv)
+      i = i + 1
+    end
+    last
+  when "var_assign"
+    lenv[tree[1]] = evaluate(tree[2], genv, lenv)
+  when "var_ref"
+    lenv[tree[1]]
+  when "func_call"
+    args = []
+    i = 0
+    while tree[i + 2]
+      args[i] = evaluate(tree[i + 2], genv, lenv)
+      i = i + 1
+    end
+    mhd = genv[tree[1]]
+    if mhd[0] == "builtin"
+      minruby_call(mhd[1], args)
+    else
+      # 埋める
+    end
+  end
+end
+
+str = minruby_load()
+tree = minruby_parse(str)
+genv = { "p" => ["builtin", "p"] }
+lenv = {}
+evaluate(tree, genv, lenv)
